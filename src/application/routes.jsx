dbc7b37fc6d8a662cbfee1ef0140167888f01535
Cristina Ferrer <krishee107@gmail.com>
2023-04-17 21:21:52 +0200

    Eliminado archivos innecesarios, build y modificado el browserrouter

--- src/application/routes.jsx
+++ src/application/routes.jsx
@@ -3,7 +3,7 @@ import Quotes from '../pages/Quotes/Quotes';
 import QuotesAuthor from '../pages/QuotesAuthor/QuotesAuthor';
 
 const Router = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/DEVCHALLENGE-quotegenerator-React/my-app">
         <Routes>
             <Route index element={<Quotes />} />
             <Route path="/author/:author" element={<QuotesAuthor />} />
